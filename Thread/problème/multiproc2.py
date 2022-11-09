dbfa1a496ca64fc5ab19202f62b66410210f170f
Grievous400 <louis.grievous@gmail.com>
2022-11-09 18:12:19 +0100

    aa

---
+++ Thread/problème/multiproc2.py
@@ -0,0 +1,17 @@
+import multiprocessing
+import requests
+img_urls = [
+    'https://cdn.pixabay.com/photo/2016/01/05/17/51/maltese-1123016_960_720.jpg'
+
+]
+def download_image(img_url):
+    img_bytes = requests.get(img_url).content
+    img_name = img_url.split('/')[9]
+    with open(img_name, 'wb') as img_file:
+        img_file.write(img_bytes)
+        print(f"{img_name} was downloaded")
+
+
+if __name__ == '__main__':
+    p1 = multiprocessing.Process(target=download_image, args=[img_urls[0]])
+    p1.start()
